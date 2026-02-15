978a9e4110de1059defe4ac3ea04c6458d7c35fb
Anton <antpklimko@gmail.com>
2026-02-15 05:20:12 +0300

    done23

--- allurerc.mjs
+++ allurerc.mjs
@@ -5,8 +5,4 @@ export default defineConfig({
     output: "./allure-report",
     historyPath: "./allure-history.jsonl",
     appendHistory: true,
-    links: {
-        report: "https://antonqa1.github.io/diploma-task3/"
-    }
-
 });
