84e66be6a8c6ccccc8786c1f0750b5484319c3c2
Anton <antpklimko@gmail.com>
2026-02-15 03:28:34 +0300

    done13

--- allurerc.mjs
+++ allurerc.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "allure";
 export default defineConfig({
     name: "Playwright Allure Report",
     output: "./allure-report",
-    baseUrl: "https://antonqa1.github.io/Diploma-task3/",
     historyPath: "./allure-history.jsonl",
     appendHistory: true
 });
